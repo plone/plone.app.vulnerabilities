171f7ad3a4c0ad99959a5e35d53585be5fca363f
Tom Blockley <tom.blockley@teamrubber.com>
2011-11-07 21:08:41 +0000

    Add in the basic cruft for adding browser views, including a simple template and backing class in preparation for adding a custom RSS feed of the Hotfix objects.

---
+++ src/plone/app/vulnerabilities/browser/hotfixes.py
@@ -0,0 +1,7 @@
+from Products.Five.browser import BrowserView
+
+class HotfixFeed(BrowserView):
+    """ Load the collection of hotfixes and perform any processing required to present 
+        the correct feed to the client """
+    
+    pass
