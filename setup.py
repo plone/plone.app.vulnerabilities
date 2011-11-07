1121811688b2fc22ac6adca0202a16d1c578482a
Matthew Wilkes <git@matthewwilkes.name>
2011-11-06 16:14:53 -0800

    Initial package and buildout

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='plone.app.vulnerabilities',
+      version=version,
+      description='',
+      long_description=open(os.path.join('src', 'plone', 'app', 'vulnerabilities', 'README.txt')).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      package_dir = {'':'src'},
+      packages=find_packages('src', exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require = {
+          'test': [
+                  'plone.app.testing',
+              ]
+      },
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'plone.app.dexterity',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
