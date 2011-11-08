918756a15a424ea866da5c73628b55ae88b94f6d
Tom Blockley <tom.blockley@teamrubber.com>
2011-11-08 01:47:12 +0000

    Add a test for publishing vulnerabilities to make them visible to the public

--- src/plone/app/vulnerabilities/tests/test_vulnerabilities.py
+++ src/plone/app/vulnerabilities/tests/test_vulnerabilities.py
@@ -5,6 +5,7 @@ from ZPublisher import NotFound
 from plone.app.testing import setRoles, TEST_USER_NAME, TEST_USER_ID
 from plone.testing.z2 import Browser
 from plone.app.testing import login, logout
+import transaction
 
 from plone.app.vulnerabilities.testing import VULN_POLICY_FUNCTIONAL_TESTING
 
@@ -21,31 +22,31 @@ class TestVulnerabilities(unittest.TestCase):
         with self.assertRaises(Unauthorized):
             browser.open(plonesite.absolute_url()+"/++add++vulnerability")
     
-    def testVulnerabilityCreation(self):
+    def test_publishing_a_vulnerability_makes_it_visible_to_anonymous(self):
         plonesite = self.layer["portal"]
         workflow = plonesite.portal_workflow
         #import pdb; pdb.set_trace( )
         
         login(plonesite, TEST_USER_NAME)
-        setRoles(plonesite, TEST_USER_ID, ["Manager"])
+        setRoles(plonesite, TEST_USER_ID, ["Manager", "Member"])
+        transaction.commit()
         plonesite.invokeFactory('vulnerability', 'v1', title=u"Vulnerability 1")
+        transaction.commit()
+        vulnerability = plonesite["v1"]
         
         app = self.layer["app"]
         browser = Browser(app)
         browser.handleErrors = False
         #import pdb; pdb.set_trace( )
         
-        
-        
         # Try navigating to a draft vulnerability as an Anonymous user
-        with self.assertRaises(NotFound):
-            browser.open(plonesite.absolute_url()+"/v1")
-            
+        with self.assertRaises(Unauthorized):
+            browser.open(vulnerability.absolute_url())
         
         # Publish the vulnerability and try visiting it again
-        #workflow.doActionFor(plonesite["v1"], 'publish')
-        #browser.open(plonesite.absolute_url()+"/v1")
-        #self.assertTrue(u"Vulnerability 1" in browser.contents)
+        workflow.doActionFor(vulnerability, 'publish')
+        transaction.commit()
         
-        # login(plonesite, TEST_USER_NAME)
-        #plonesite.restrictedTraverse("++add++vulnerability")
+        browser.open(vulnerability.absolute_url())
+        self.assertTrue(u"Vulnerability 1" in browser.contents)
+        
