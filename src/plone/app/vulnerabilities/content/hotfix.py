00ea74b5127189da3e25651f43026e60f892aa25
Alan Hoey <alan.hoey@gmail.com>
2013-08-11 16:54:44 +0200

    Allow hotfixes to contain objects, and compute their initial ID from the release date.

--- src/plone/app/vulnerabilities/content/hotfix.py
+++ src/plone/app/vulnerabilities/content/hotfix.py
@@ -1,12 +1,14 @@
 from zope.interface import Interface, implements
-from plone.dexterity.content import Item
+from plone.dexterity.content import Container
 from zope import schema
 from plone.app.vulnerabilities import VulnerabilitiesMessageFactory as _
+from plone.app.content.interfaces import INameFromTitle
+
 
 class IHotfix(Interface):
     """ Marker interface for Hotfixes """
 
-    release_date = schema.Datetime(title=_(u"Release date"),
+    release_date = schema.Date(title=_(u"Release date"),
                                    description=_(u"Date the hotfix was released"),
                                    )
 
@@ -16,5 +18,17 @@ class IHotfix(Interface):
 
     # XXX: Original schema contained a hotfix_version field, which made no sense.
 
-class Hotfix(Item):
+class NameFromReleaseDate(object):
+    implements(INameFromTitle)
+    
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def title(self):
+        # Hotfixes have their ID generated from their release date.
+        return self.context.release_date.strftime("%Y%m%d")
+    
+class Hotfix(Container):
     implements(IHotfix)
+    
