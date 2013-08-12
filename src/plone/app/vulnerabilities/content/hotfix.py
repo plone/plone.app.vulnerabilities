fb9b7f07ef8d7921d7c73df99299583dcefc14a5
Alan Hoey <alan.hoey@gmail.com>
2013-08-12 13:39:08 +0200

    Add some field permissions, will tidy this up later

--- src/plone/app/vulnerabilities/content/hotfix.py
+++ src/plone/app/vulnerabilities/content/hotfix.py
@@ -5,6 +5,7 @@ from plone.app.vulnerabilities import VulnerabilitiesMessageFactory as _
 from plone.app.content.interfaces import INameFromTitle
 from plone.app.textfield import RichText
 from plone.supermodel import model
+from plone.autoform.directives import read_permission
 
 from plone.app.vulnerabilities.field import ChecksummedFile
 class IHotfix(model.Schema):
@@ -18,13 +19,16 @@ class IHotfix(model.Schema):
                                description=_(u"Date the hotfix will be released"))
 
     # XXX: What about non-core packages?
+    read_permission(affected_versions='plone.app.vulnerabilities.hotfix.view_release')
     affected_versions = schema.List(title=_(u"Affected Plone versions"),
                                     value_type=schema.Choice(source="plone.app.vulnerabilities.ploneversions"))
 
+    read_permission(hotfix='plone.app.vulnerabilities.hotfix.view_release')
     hotfix = ChecksummedFile(title=_(u"Hotfix"),
                         description=_(u"Old-style product tarball for this hotfix"),
                         required=False)
     
+    read_permission(text='plone.app.vulnerabilities.hotfix.view_release')
     text = RichText(title=_(u"Release body"),
                     description=_(u"This will be shown after the hotfix is released"),
                     default=u"",
@@ -37,6 +41,7 @@ class IHotfix(model.Schema):
             fields=['preannounce_text']
         )
 
+    read_permission(preannounce_text='plone.app.vulnerabilities.hotfix.view_preannounce')
     preannounce_text = RichText(title=_(u"Preannounce body"),
                                 description=_(u"This will be shown while the hotfix is in the preannounce state"),
                                 default=u"",
