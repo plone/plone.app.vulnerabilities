e57580efc6fa1ee937dbd44d04aa35e2e79d9b11
Alan Hoey <alan.hoey@gmail.com>
2013-08-12 12:22:50 +0200

    Adding experimental implementation of file field with checksums

---
+++ src/plone/app/vulnerabilities/field.py
@@ -0,0 +1,48 @@
+from hashlib import sha1, md5
+from zope.interface import implements
+from zope import schema
+from zope.schema.fieldproperty import FieldProperty
+from plone.namedfile.interfaces import INamedFile, INamedFileField
+from plone.namedfile.field import NamedFile
+from plone.namedfile.file import NamedFile as FileValueType
+
+# I have no idea what I am doing here.
+
+class IChecksummedFile(INamedFile):
+    md5 = schema.TextLine(title=u"MD5", required=False, default=None)
+    sha1 = schema.TextLine(title=u"SHA1", required=False, default=None)
+    
+class IChecksummedFileField(INamedFileField):
+    pass
+
+class ChecksummedFileValueType(FileValueType):
+
+    implements(IChecksummedFile)
+
+    md5 = FieldProperty(IChecksummedFile['md5'])
+    sha1 = FieldProperty(IChecksummedFile['sha1'])
+
+    def __init__(self, **kw):
+        super(ChecksummedFileValueType,self).__init__(**kw)
+        
+    def _setData(self,data):
+        super(ChecksummedFileValueType,self)._setData(data)
+        body = self._getData()
+        self.md5 = unicode(sha1(body).hexdigest())
+        self.sha1 = unicode(md5(body).hexdigest())
+
+    data = property(FileValueType._getData, _setData)
+
+class ChecksummedFile(NamedFile):
+    """ A file field which computes MD5 and SHA1 checksums for the uploaded file """
+    implements(IChecksummedFileField)
+    
+    _type = ChecksummedFileValueType
+    schema = IChecksummedFile
+    
+    def __init__(self, **kw):
+        if 'schema' in kw:
+            self.schema = kw.pop('schema')
+        super(ChecksummedFile, self).__init__(schema=self.schema, **kw)
+    
+    
