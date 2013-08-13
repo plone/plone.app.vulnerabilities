b4d60970735ed5112a0ff12661192a361662ad4e
Ramon Navarro Bosch <ramon.nb@gmail.com>
2013-08-13 11:19:09 +0200

    Portlets nicer UI

--- src/plone/app/vulnerabilities/browser/hotfixes.py
+++ src/plone/app/vulnerabilities/browser/hotfixes.py
@@ -17,6 +17,16 @@ class HostfixListing(BrowserView):
     """ Load the collection of hotfixes and perform any processing required to present 
         the correct list to the client """
 
+    def get_hotfixes(self):
+        context = aq_inner(self.context)
+        tools = getMultiAdapter((context, self.request), name=u'plone_tools')
+
+        portal_catalog = tools.catalog()
+        brains = portal_catalog(object_provides=IHotfix.__identifier__)
+
+        return sorted(result, key=lambda hotfix: hotfix.id, reverse=True)
+
+
     def get_versions(self):
         registry = getUtility(IRegistry)
         versions = registry['plone.versions']
