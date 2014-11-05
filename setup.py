83b67b53e160558acd9e0e84a67c07c7e45812ec
Jean Jordaan <jean.jordaan@gmail.com>
2014-11-05 22:44:14 +0000

    There's only one top-level README

--- setup.py
+++ setup.py
@@ -6,10 +6,7 @@ version = '0.1'
 setup(name='plone.app.vulnerabilities',
       version=version,
       description='',
-      long_description='\n\n'.join([
-            open('README.rst').read(),
-            open(os.path.join('src', 'plone', 'app', 'vulnerabilities', 'README.txt')).read()
-      ]),
+      long_description=open('README.rst').read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
